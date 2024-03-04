df722923485adc52b208f923897077e50e1711cc
Ayub40 <ayubk4028@gmail.com>
2024-03-05 03:41:57 +0600

    added some functional in  lets section

--- js/main.js
+++ js/main.js
@@ -1,50 +1,60 @@
-// const showTooltip = event => {
-//     const tooltip = document.createElement('div');
-//     tooltip.textContent = 'home';
-//     tooltip.style.cssText = 'position: absolute; background-color: #555; color: #fff; padding: 5px; border-radius: 6px;';
-//     tooltip.style.left = event.pageX + 'px';
-//     tooltip.style.top = event.pageY + 'px';
-//     document.body.appendChild(tooltip);
-//     event.target._tooltip = tooltip;
-// };
+const loadNews = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
+    const data = await res.json();
+    // console.log(data.posts);
+    // console.log(data.posts[0].id);
+    const allData = data.posts;
+    const newsContainer = document.getElementById('news-container');
+    allData.forEach((item) => {
+        const div = document.createElement("div");
+        div.classList.add("single-news");
+        div.innerHTML = `
+        <div
+        class="relative card card-side bg-[#F3F3F5] sm:pl-1 lg:pl-6 flex flex-col lg:flex-row border">
+        <figure><img class="absolute left-4 top-7 w-16 lg:w-16 rounded-full" src="${item.image}" /></figure>
 
-// const hideTooltip = event => {
-//     if (event.target._tooltip) {
-//         document.body.removeChild(event.target._tooltip);
-//         event.target._tooltip = null;
-//     }
-// };
-
-// const homeImage = document.getElementById('home');
-// homeImage.addEventListener('mouseover', showTooltip);
-// homeImage.addEventListener('mouseout', hideTooltip);
-
-// const homeImage = document.getElementById('home');
-// homeImage.setAttribute('img', 'Home');
+        <div class=" card-body ml-8  rounded-lg">
 
 
+            <div class="card-actions gap-6">
+                <h3 class="text-lg font-extrabold ">${item.category}</h3>
+                <div class="flex gap-3">
+                    <img src="Images/Frame (2).png" alt="">
+                    <p>Author : ${item.author.name}</p>
+                </div>
+            </div>
+            <h2 class="card-title">${item.title}</h2>
+            <p>${item.description} </p>
 
-// const showTooltip = event => {
-//     const tooltip = document.createElement('div');
-//     tooltip.textContent = event.target.dataset.tooltip;
-//     tooltip.style.cssText = 'position: absolute; background-color: #555; color: #fff; padding: 5px; border-radius: 6px;';
-//     tooltip.style.left = event.pageX + 'px';
-//     tooltip.style.top = event.pageY + 'px';
-//     document.body.appendChild(tooltip);
-//     event.target._tooltip = tooltip;
-// };
+            <div class="flex gap-4">
+                <div class="rating gap-2">
 
-// const hideTooltip = event => {
-//     if (event.target._tooltip) {
-//         document.body.removeChild(event.target._tooltip);
-//         event.target._tooltip = null;
-//     }
-// };
+                    <img src="images/tabler-icon-message-2.svg" alt="">
+                    <p>${item.comment_count}</p>
+                </div>
+                <div class="flex gap-2">
 
-// const images = document.querySelectorAll('.title');
-// const tooltips = ['Home', 'Edit', 'People', 'Flag'];
-// images.forEach((img, index) => {
-//     img.dataset.tooltip = tooltips[index];
-//     img.addEventListener('mouseover', showTooltip);
-//     img.addEventListener('mouseout', hideTooltip);
-// });
+                    <img src="images/tabler-icon-eye.svg" alt="">
+                    <p>${item.view_count}</p>
+                </div>
+                <div class="flex gap-2">
+
+                    <img src="images/tabler-icon-clock-hour-9.svg" alt="">
+                    <p>${item.posted_time}</p>
+                </div>
+                <div class="flex gap-2">
+
+                    <img src="images/email 1.svg" alt="">
+
+                </div>
+            </div>
+        </div>
+    </div>
+        
+        `;
+        newsContainer.appendChild(div);
+
+    });
+}
+
+loadNews()
